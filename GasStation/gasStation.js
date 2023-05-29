b6c6ed7a0a4426f06d1453bc076bd122042c702d
Rafael Canabrava <rafaelxcana@gmail.com>
2023-05-28 23:03:51 -0300

    Adicionando loop para achar a solucao

--- GasStation/gasStation.js
+++ GasStation/gasStation.js
@@ -8,6 +8,18 @@ var canCompleteCircuit = function(gas, custo) {
     let totalGas = 0;
     let atualGas = 0;
     let inicio = 0;
-  
+    
+    for (let i = 0; i < n; i++) {
+        totalGas += gas[i] - custo[i];
+        atualGas += gas[i] - custo[i];
+        
+        if (atualGas < 0) {
+          inicio = i + 1;
+          atualGas = 0;
+        }
+      }
+    
+      return totalGas >= 0 ? inicio : -1;
+
     return 0;
   };

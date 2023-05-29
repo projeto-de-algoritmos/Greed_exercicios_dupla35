cfd338c539b3af2608406b8cd4ea853dc417c987
Rafael Canabrava <rafaelxcana@gmail.com>
2023-05-28 22:59:56 -0300

    Criando a funcao e declarando variaveis

---
+++ GasStation/gasStation.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} gas
+ * @param {number[]} custo
+ * @return {number}
+ */
+var canCompleteCircuit = function(gas, custo) {
+    const n = gas.length;
+    let totalGas = 0;
+    let atualGas = 0;
+    let inicio = 0;
+  
+    return 0;
+  };
